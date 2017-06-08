f2f402471e231afb923acc13cf305c9ec445d8c6
Jan Parthemore <janparthemore@gmail.com>
2017-06-08 19:17:47 -0400

    implemented add todo method so people can add todos to list. cleaned upthe mock.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
 /*jshint esversion: 6*/
-const mockTodo = {
-  task:'get some milk',
-  isComplete:false
-};
+
 const TodoApp = {
   rootElement: '#app',
-  todos: [mockTodo],
+  todos: [],
   start: function(){
     this.cacheDOM();
     this.bindEvents();
@@ -18,7 +15,27 @@ const TodoApp = {
     this.todoList = this.root.querySelector('.todo-list');
    },
   bindEvents: function(){
-    this.addButton.addEventListener('click',()=>console.log('add'));//dont need curly braces for one inline liner
+    this.addButton.addEventListener('click',()=>this.addTodo());//dont need curly braces for one inline liner
+  },
+  addTodo: function(){
+    //first grab task input value
+    const taskValue = this.taskInput.value;
+    //1b - validate that taskValue is actually'something'
+    if(!taskValue){
+      //alert('NO');
+      return;
+    }
+    //2nd buld a todo object w/that value
+    const todo = {
+      task: taskValue,
+      isComplete: false
+    };
+    //3 - add that toodo to todos array
+    this.todos.push(todo);
+    //4 - rerender
+    this.render();
+    //5 - clear input
+    this.taskInput.value = '';
   },
   render: function(){
     //const lis = this.todos.map(todo=>todo.task);
@@ -26,8 +43,8 @@ const TodoApp = {
     //console.log(lis);
     const lis = this.todos
                     .map(todo=>`<li>${todo.task}</li>`)
-                    .join();
-    this.todoList.innerHTML = lis;
+                    .join('');
+    this.todoList.innerHTML = lis;//be careful can lead to security vulnerabilities
     }
 
 };
